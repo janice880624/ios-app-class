a23c42f127749adce417dfaccc5e873b83b09803
janice880624 <t106300302@ntut.org.tw>
2021-06-19 12:03:58 +0800

    0619

--- LogIn/LogIn/ViewController.m
+++ LogIn/LogIn/ViewController.m
@@ -33,18 +33,29 @@
     NSString *InputPassword = self.passwordTextField.text;
     
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"登入" message:[NSString stringWithFormat:@"%@", InputId] preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertController *alertController1 = [UIAlertController alertControllerWithTitle:@"大標題" message:@"小標題" preferredStyle:UIAlertControllerStyleAlert];
     
     UIAlertAction* ShowButton = nil;
     
     if([self.username isEqualToString: InputId] && [self.password isEqualToString: InputPassword]){
         [alertController setMessage:[NSString stringWithFormat: @"%@ 登入成功", InputId]];
-        ShowButton = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *_Nonnull action){
+        ShowButton = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){
+//            LoginSucess *loginsucess = [[LoginSucess alloc] initWithNibName: @"LoginSucess" bundle:nil];
+//            loginsucess.userid = InputId;
+//            loginsucess.userped = InputPassword;
+            
             NSLog(@"OK hander");
+//            [self presentViewController:loginsucess animated:TRUE completion:nil]
+        }];
+    }else{
+        [alertController setMessage:[NSString stringWithFormat:@"%@ 登入失敗", InputId]];
+        ShowButton = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){
         }];
     }
     
     [alertController addAction: ShowButton];
     [self presentViewController:alertController animated:YES completion:nil];
+    [self presentViewController:alertController1 animated:TRUE completion:nil];
 }
 
 @end
